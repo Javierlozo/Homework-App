c16eeb41782e72deda214f9d7f2a55a0f604c4ec
Luis Javier Lozoya <luisloart@gmail.com>
2020-08-12 20:22:15 -0400

    added NavBar and SignUp Modal

---
+++ src/components/pages/NavBar.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import SignUp from "./SignUp";
+import Modal from "@material-ui/core/Modal";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appbar: {
+    backgroundColor: "black",
+    opacity: 0.5,
+  },
+}));
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appbar} position="fixed">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}></Typography>
+          <Button color="inherit" onClick={handleOpen}>
+            Sign Up
+          </Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+          >
+            <SignUp />
+          </Modal>
+          <Button color="inherit">Log In</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
