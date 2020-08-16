bb24aba8ce1abe751b2e27d686286df8e66fa240
Janita M <jaymadr@gmail.com>
2020-08-16 15:22:44 -0400

    created studentDashboard

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
-import "./App.css";
-import NavBar from "./components/pages/NavBar";
-import SignUp from "./components/pages/SignUp";
+import React, { Component } from 'react';
+import './App.css';
+import NavBar from './components/pages/NavBar';
+import SignUp from './components/pages/SignUp';
+import StudentDashboard from './components/studentDashboard/StudentDashboard';
 // import UserForm from "./components/pages/UserForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <NavBar />
         {/* <SignUp /> */}
         {/* <UserForm /> */}
+        <StudentDashboard />
       </header>
     </div>
   );
