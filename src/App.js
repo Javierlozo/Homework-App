d9e44219f45fc9dc3dbe76a8980bf19b5e94ce28
Nitika Chaudhary <nitika.hopkins@gmail.com>
2020-08-22 21:09:36 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { Router } from "@reach/router";
 import "./App.css";
 import NavBar from "./components/pages/NavBar";
 import StudentDashboard from "./components/studentDashboard/StudentDashboard";
 import TeacherDashboard from "./components/teacherDashboard/TeacherDashboard";
-// import UserForm from "./components/pages/UserForm";
 import Home from "./components/pages/Home";
+import TheFooter from "./components/TheFooter";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+
       <Router>
         <Home path="/"></Home>
         <StudentDashboard path="/studentdashboard" />
         <TeacherDashboard path="/teacherdashboard" />
       </Router>
+      <TheFooter />
     </div>
   );
 }
