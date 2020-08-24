4f0146651b392de841256c279d163faa17efe8d6
Florence Braddy <florencebraddy@hotmail.com>
2020-08-23 21:39:17 -0400

    background image styling for home page

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-
       <Router>
         <Home path="/"></Home>
         <StudentDashboard path="/studentdashboard" />
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,34 +1,19 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-// import ImageBackground from "react";
 
-// const image = {
-//   uri:
-//     "https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-// };
 const useStyles = makeStyles(theme => ({
   home: {
     backgroundImage: `url("https://images.unsplash.com/photo-1516979187457-637abb4f9353?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")`,
     backgroundRepeat: "no-repeat",
-    backgroundSize: "contain",
-    width: "100vw",
-    height: "100vw"
-    // backgroundPosition: "center",
-    // backgroundAttachment: "fixed"
+    backgroundSize: "cover",
+    // width: "100vw",
+    height: "100vw",
+    backgroundPosition: "center",
+    backgroundAttachment: "fixed"
   }
 }));
 
-// const Home = () => {
-//   return <div>Home</div>;
-// };
-
-// export default Home;
-
 export default function Home() {
   const classes = useStyles();
-  return (
-    // <ImageBackground source={image} className={classes.image}>
-    <div className={classes.home}>Home</div>
-    // </ImageBackground>
-  );
+  return <div className={classes.home}>Home</div>;
 }
